4fc648b3dbecc9ab7f5fd4e32e6a953b75258941
SoominYim <soo15soo@kakao.com>
2025-02-25 10:52:24 +0900

    feat : counter module update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import "./App.css";
-import store from "./modules/config/configStore";
 import { Provider } from "react-redux";
-import { useSelector } from "react-redux";
-
-function Main() {
-  const counterStore = useSelector((state) => state); // 스토어를 조회합니다.
-  console.log(counterStore);
-  return <main></main>;
-}
+import store from "./store";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <Provider store={store}>
-      <Main /> {/* Provider 내부에서 useSelector를 사용하는 컴포넌트를 추가 */}
+      <Counter />
     </Provider>
   );
 }
